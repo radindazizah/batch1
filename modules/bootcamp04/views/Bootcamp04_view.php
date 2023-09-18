f459862e4a554373cf459e13a66a527ec84e0fa7
albert <albert@bootcamp01.com>
2023-09-18 17:19:21 +0700

    [bootcamp04] perubahan pada folder models,views,controllers

--- modules/bootcamp04/views/Bootcamp04_view.php
+++ modules/bootcamp04/views/Bootcamp04_view.php
@@ -1,17 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-        <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width" />
-        <title>Bootcamp04</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Bootcamp04</title>
+    <link rel="stylesheet" href="<?= base_url() ?>modules/bootcamp04/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>modules/bootcamp04/css/jquery-ui.css">
+    <link rel="stylesheet" href="<?= base_url() ?>modules/bootcamp04/css/ui.jqgrid.css">
+    <!-- Add any additional CSS here -->
+</head>
 <body>
-<?php
+    <div id="content">
+        <?php echo "Username: " . $user; ?>
+        <table id="userKaryawan"></table>
+        <div id="userKaryawanPager"></div>
+    </div>
 
-?>
-<script type="text/javascript">
-	
-</script>
-    <h1>hello world</h1> 
+    <script>
+        var USER_ID = '<?= $user ?>';
+    </script>
+
+    <!-- Load JavaScript from external files -->
+    <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="<?= base_url() ?>modules/bootcamp04/js/jqGrid/jquery.jqGrid.js"></script>
+    <script src="<?= base_url() ?>modules/bootcamp04/js/jqGrid/i18n/grid.locale-en.js"></script>
+    <script src="<?= base_url() ?>modules/bootcamp04/js/Bootcamp04.js?v=<?= time(); ?>"></script>
+    <!-- Add any additional JavaScript files here -->
 </body>
 </html>

1c8e6ad9727048bd16c006f3fcf293677db7e7dc
albert <albert@bootcamp01.com>
2023-09-18 13:01:24 +0700

    [bootcamp04] penambahan folder views dan models

---
+++ modules/bootcamp04/views/Bootcamp04_view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width" />
+        <title>Bootcamp04</title>
+    </head>
+<body>
+<?php
+
+?>
+<script type="text/javascript">
+	
+</script>
+    <h1>hello world</h1> 
+</body>
+</html>

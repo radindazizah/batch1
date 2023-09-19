aa1c6490fb950e5f8a4536d6acb10e11a1fe5be0
Hendri Narendra <hendrinarendra@gmail.com>
2023-09-19 19:52:04 +0700

    [bootcamp03] Penambahan form untuk menambahkan data dan add edit delete button

--- modules/bootcamp03/views/Bootcamp03_view.php
+++ modules/bootcamp03/views/Bootcamp03_view.php
@@ -8,19 +8,75 @@
     <link href="<?= base_url() ?>modules/bootcamp03/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?= base_url() ?>modules/bootcamp03/css/jquery-ui.css" />
     <link rel="stylesheet" href="<?= base_url() ?>modules/bootcamp03/css/ui.jqgrid.css" />
+    <link rel="stylesheet" href="<?= base_url() ?>modules/bootcamp03/css/style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 
 <body>
-    <?php
-    echo "username : " . $user;
+    <div class="container">
+        <?php
+        echo "<p>Username : " . $user . "</p>";
+        ?>
 
-    ?>
+        <table id="userKaryawan"></table>
+        <div id="userKaryawanPager"></div>
 
-    <table id="userKaryawan"></table>
-    <div id="userKaryawanPager"></div>
+        <div class="button-wrapper">
+            <button type="button" class="btn btn-primary" id="show-add">Add</button>
+            <button type="button" class="btn btn-warning">Edit</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+
+        <div class="row form-wrapper">
+            <div class="col-lg-3 add-form-wrapper">
+                <form>
+                    <div class="form-group">
+                        <label for="nik">NIK</label>
+                        <input type="number" class="form-control" id="nik" placeholder="NIK">
+                    </div>
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" placeholder="Nama">
+                    </div>
+                    <div class="form-group">
+                        <label for="tempat-lahir">Tempat Lahir</label>
+                        <input type="text" class="form-control" id="tempat-lahir" placeholder="Tempat Lahir">
+                    </div>
+                    <div class="form-group">
+                        <label for="umur">Umur</label>
+                        <input type="number" class="form-control" id="umur" placeholder="Umur">
+                    </div>
+                    <div class="form-group">
+                        <label for="alamat">Alamat</label>
+                        <input type="text" class="form-control" id="alamat" placeholder="Alamat">
+                    </div>
+                    <div class="form-group">
+                        <label for="telp">Telp</label>
+                        <input type="text" class="form-control" id="telp" placeholder="No. Telepon">
+                    </div>
+                    <div class="form-group">
+                        <label for="jabatan">Jabatan</label>
+                        <input type="text" class="form-control" id="jabatan" placeholder="Jabatan">
+                    </div>
+                    <div class="btn-submit-wrapper">
+                        <button type="submit" class="btn btn-primary btn-submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+    </div>
 
     <script type="text/javascript">
         var USER_ID = '<?= $user ?>';
+
+        $(document).ready(function() {
+            $(".add-form-wrapper").hide();
+            $("#show-add").click(function() {
+                $(".add-form-wrapper").show();
+            });
+        });
     </script>
 
     <script src='<?= base_url() ?>modules/bootcamp03/js/jquery-2.0.3.min.js'></script>

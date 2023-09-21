04b198b151fea0946c76510f9f3177e50d42e43d
Hendri Narendra <hendrinarendra@gmail.com>
2023-09-21 15:30:58 +0700

    [bootcamp03] Penambhan validasi untuk duplikat NIK

--- modules/bootcamp03/controllers/Bootcamp03.php
+++ modules/bootcamp03/controllers/Bootcamp03.php
@@ -3,7 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Bootcamp03 extends CI_Controller
 {
-
 	function __construct()
 	{
 		parent::__construct($securePage = false);
@@ -16,7 +15,9 @@ class Bootcamp03 extends CI_Controller
 	{
 		$data['user'] = $this->input->get_post('id');
 		$data['karyawan'] = $this->Bootcamp03_model->getKaryawan();
-		
+
+
+		$this->session->set_userdata('user_session', $data['user']);
 		$this->load->view('Bootcamp03_view', $data);
 	}
 
@@ -28,9 +29,12 @@ class Bootcamp03 extends CI_Controller
 
 	public function addKaryawan()
 	{
-		// store user into session 
+		// store currrent user into session 
 		$data['user'] = $this->input->get_post('id');
 		$this->session->set_userdata('user_session', $data['user']);
+		$user_session = $this->session->userdata('user_session');
+
+		$data = $this->Bootcamp03_model->addKaryawan();
 
 		// form validation
 		$rules = $this->Bootcamp03_model->rules();
@@ -38,12 +42,18 @@ class Bootcamp03 extends CI_Controller
 
 		// changing delimiters globally for adding styles
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-		
+
 		if ($this->form_validation->run() == FALSE) {
 			$this->load->view('Bootcamp03_view', $data);
 		} else {
 			$data = $this->Bootcamp03_model->addKaryawan();
-			redirect('bootcamp03/?id=' . $_SESSION['user_session']);
+			redirect('bootcamp03/?id=' . $user_session);
 		}
+	}
+
+	public function delKaryawan($nik) {
+		$where = array('nik' => $nik);
+		$this->Bootcamp03_model->delKaryawan($where);
+		redirect('bootcamp03/?id=' . $_SESSION['user_session']);
 	}
 }
--- modules/bootcamp03/js/Bootcamp03.js
+++ modules/bootcamp03/js/Bootcamp03.js
@@ -1,59 +1,65 @@
-var select=false;
+var select = false;
 var grid_selector = $("#userKaryawan");
 var pager_selector = "#userKaryawanPager";
 
+$(".add-form-wrapper").hide();
+
+$("#show-add").click(function () {
+	$(".add-form-wrapper").toggle();
+});
+
 grid_selector.jqGrid({
-	jsonReader : { 
-		root: "rows", 
-		page: "page", 
-		total: "total", 
-		records: "records", 
-		repeatitems: true, 
-		cell: "cell", 
+	jsonReader: {
+		root: "rows",
+		page: "page",
+		total: "total",
+		records: "records",
+		repeatitems: true,
+		cell: "cell",
 		id: "id",
 		userdata: "userdata",
-		subgrid: { 
-		   root:"rows", 
-		   repeatitems: true, 
-		   cell:"cell" 
-		} 
-	 },	
-	url: 'bootcamp03/getKaryawan/?id='+USER_ID,
-	mtype : "post",
+		subgrid: {
+			root: "rows",
+			repeatitems: true,
+			cell: "cell"
+		}
+	},
+	url: 'bootcamp03/getKaryawan/?id=' + USER_ID,
+	mtype: "post",
 	datatype: "json",
 	height: 394,
-	colNames:['NIK','Nama','Tempat Lahir','Tanggal Lahir','Umur', 'Alamat', 'Telp', 'Jabatan', 'Created By', 'Created Time'],
-	colModel:[
-		{name: 'nik', width: 150, sortable: false },
-		{name: 'nama', width: 150, sortable: false, editable: true, editrules: {required: true} ,editoptions: {maxlength: 50} },
-		{name: 'tempat_lahir', width: 150, sortable: false, editable: true, editrules: {required: false}},
-		{name: 'tanggal_lahir', width: 150, sortable: false, editable: true, editrules: {required: false}},
-		{name: 'umur', width: 150, sortable: false, editable: true, editrules: {required: false}},
-		{name: 'alamat', width: 150, sortable: false, editable: true, editrules: {required: false}},
-		{name: 'telp', width: 150, sortable: false, editable: true, editrules: {required: false}},
-		{name: 'jabatan', width: 150, sortable: false, editable: true, editrules: {required: false}},
-		{name: 'created_by', width: 150, sorttype: false, editable: false },
-		{name: 'created_time', width: 150, sorttype: false, editable: false },
-		
-	], 
+	colNames: ['NIK', 'Nama', 'Tempat Lahir', 'Tanggal Lahir', 'Umur', 'Alamat', 'Telp', 'Jabatan', 'Created By', 'Created Time'],
+	colModel: [
+		{ name: 'nik', width: 150, sortable: false },
+		{ name: 'nama', width: 150, sortable: false, editable: true, editrules: { required: true }, editoptions: { maxlength: 50 } },
+		{ name: 'tempat_lahir', width: 150, sortable: false, editable: true, editrules: { required: false } },
+		{ name: 'tanggal_lahir', width: 150, sortable: false, editable: true, editrules: { required: false } },
+		{ name: 'umur', width: 150, sortable: false, editable: true, editrules: { required: false } },
+		{ name: 'alamat', width: 150, sortable: false, editable: true, editrules: { required: false } },
+		{ name: 'telp', width: 150, sortable: false, editable: true, editrules: { required: false } },
+		{ name: 'jabatan', width: 150, sortable: false, editable: true, editrules: { required: false } },
+		{ name: 'created_by', width: 150, sorttype: false, editable: false },
+		{ name: 'created_time', width: 150, sorttype: false, editable: false },
 
-	shrinkToFit : false,
-	viewrecords : true,
-	rowNum:10,
-	rowList:[10,20,30],
-	pager : pager_selector,
+	],
+
+	shrinkToFit: false,
+	viewrecords: true,
+	rowNum: 10,
+	rowList: [10, 20, 30],
+	pager: pager_selector,
 	altRows: true,
 	//toppager: true,
-	
+
 	//multiselect: true,
 	//multikey: "ctrlKey",
 	multiboxonly: true,
-	onSelectRow: function(data){
-		select=true;
+	onSelectRow: function (data) {
+		select = true;
 	},
-	loadComplete : function() {
+	loadComplete: function () {
 		var table = this;
-		setTimeout(function(){
+		setTimeout(function () {
 			styleCheckbox(table);
 			updateActionIcons(table);
 			updatePagerIcons(table);
@@ -61,10 +67,66 @@ grid_selector.jqGrid({
 		}, 0);
 	},
 
-	
+
 	caption: "Data Karyawan",
 
-	
+
 	autowidth: true
 
 });
+
+function getSelectedRow() {
+	var selRowId = grid_selector.jqGrid('getGridParam', 'selrow');
+	var celValue = grid_selector.jqGrid('getCell', selRowId, 'nik');
+
+	// if (selRowId) {
+	// 	$("#delButton").attr("data-toggle", "modal");
+	// 	$("#delButton").attr("data-target", "#exampleModalLong");
+	// } else {
+	// 	alert('NO ROW SELECTED');
+	// }
+
+	if (selRowId) {
+		console.log("Selected row primary key is: " + celValue);
+		// $( "#dialog-confirm" ).append( "<p>Test</p>" );
+		$("#dialog-confirm").html(
+			'<p class="modal-delete"><span class="ui-icon ui-icon-alert" style="float:left; margin:12px 12px 20px 0;"></span>Data with nik ' + celValue + 'will be permanently deleted and cannot be recovered. Are you sure?</p>'
+		);
+		$("#dialog-confirm").dialog({
+			resizable: false,
+			height: "auto",
+			width: 400,
+			modal: true,
+			buttons: {
+				"Delete all items": function () {
+					window.location = "www.example.com/index.php?id=" + celValue;
+					$(this).dialog("close");
+				},
+				Cancel: function () {
+					$(this).dialog("close");
+				}
+			}
+		});
+	}
+	else {
+		alert("No rows are selected");
+	}
+}
+
+// Jquery UI Modal
+// $(function () {
+// 	$("#dialog-confirm").dialog({
+// 		resizable: false,
+// 		height: "auto",
+// 		width: 800,
+// 		modal: true,
+// 		buttons: {
+// 			"Delete all items": function () {
+// 				$(this).dialog("close");
+// 			},
+// 			Cancel: function () {
+// 				$(this).dialog("close");
+// 			}
+// 		}
+// 	});
+// });
